6efa8449fd1a50e83e515eb006942ec9a89c6eec
Luis Carlos <cd210902@gmail.com>
2023-06-02 11:50:25 -0700

    Cambios

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+ function AboutPage(){
+    return(
+        <div>
+            <h1>About</h1>
+            <p>Duis cillum nulla irure anim qui dolore cillum. Aute velit labore fugiat aute proident labore incididunt deserunt velit esse minim culpa eu nulla. Officia aliqua proident esse velit consequat nostrud deserunt dolore. Veniam quis labore fugiat aliquip voluptate sint consequat velit officia adipisicing.</p>
+        </div>
+    )
+ }
+
+ export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,15 @@
+function ServicesPage() {
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>
+        Nisi ea proident in deserunt occaecat consequat fugiat commodo cupidatat
+        nostrud consequat. Adipisicing ut labore fugiat nulla excepteur ea
+        cillum. Cupidatat fugiat quis laboris consectetur proident cupidatat
+        ipsum. Laborum ullamco non sunt dolor dolore velit veniam ex nisi.
+      </p>
+    </div>
+  );
+}
+
+export default ServicesPage;
